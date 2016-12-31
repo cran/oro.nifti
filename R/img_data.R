6c784eef4ddce7a60bc57aac10623c870cb87136
Brandon Whitcher <bwhitcher@gmail.com>
2016-12-31 15:32:14 +0000

    version 0.7.2

---
+++ R/img_data.R
@@ -0,0 +1,80 @@
+#' @name img_data-methods
+#' @title Extract Image Attribute \code{.Data}
+#' @docType methods 
+#' @param object is an object of class \code{nifti} or \code{anlz}.
+#' @param value is the value to assign to the \code{.Data} field.  
+#' @description Methods that act on the \code{.Data} field in the
+#' NIfTI/ANALYZE header.
+#' @rdname img_data-methods
+#' @aliases img_data-methods, img_data
+#' @details See documentation on the ANALYZE and/or NIfTI data standards for
+#' more details.
+#' @author John Muschelli \email{muschellij2@@gmail.com},\cr
+#' Brandon Whitcher \email{bwhitcher@@gmail.com}
+#' @references
+#' ANALYZE 7.5\cr
+#' \url{http://www.mayo.edu/bir/PDF/ANALYZE75.pdf}\cr
+#' NIfTI-1\cr
+#' \url{http://nifti.nimh.nih.gov/}
+#'
+#' @export
+setGeneric("img_data", function(object) standardGeneric("img_data"))
+#' @rdname img_data-methods
+#' @aliases img_data,nifti-method
+#' @export
+setMethod("img_data", "nifti", function(object) { object@".Data" })
+#' @rdname img_data-methods
+#' @aliases img_data,anlz-method
+#' @export
+setMethod("img_data", "anlz", function(object) { object@".Data" })
+#' @rdname img_data-methods
+#' @aliases img_data,character-method
+#' @export
+setMethod("img_data", "character", function(object) { 
+  object = readNifti(object)
+  as.array(object)
+  })
+#' @rdname img_data-methods
+#' @aliases img_data,ANY-method
+#' @importFrom RNifti retrieveNifti
+#' @export
+setMethod("img_data", "ANY", function(object) { 
+  object = RNifti::retrieveNifti(object)
+  as.array(object)
+})
+
+#' @rdname img_data-methods
+#' @aliases img_data<- 
+#' @export
+setGeneric("img_data<-", function(object, value) { standardGeneric("img_data<-") })
+#' @rdname img_data-methods
+#' @aliases img_data<-,nifti-method
+#' @export
+setMethod("img_data<-", 
+          signature(object = "nifti"), 
+          function(object, value) { 
+            if ( ".Data" %in% slotNames(object) ) {
+              object@".Data" <- value
+              object = cal_img(object)
+              audit.trail(object) <-
+                niftiAuditTrailEvent(object, "modification", match.call(),
+                                     paste(".Data <-", value))               
+            } else {
+              warning(".Data is not in slotNames of object")
+            }                       
+            return(object)
+          })
+#' @rdname img_data-methods
+#' @aliases img_data<-,anlz-method
+#' @export
+setMethod("img_data<-", 
+          signature(object = "anlz"), 
+          function(object, value) { 
+            if ( ".Data" %in% slotNames(object) ) {
+              object@".Data" <- value
+              object = cal_img(object)
+            } else {
+              warning(".Data is not in slotNames of object")
+            }
+            return(object)
+          })
